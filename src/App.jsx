e7f8275cf00580956285173954346cfdcfd8dbfb
Ondra Svec <ondrasvecveslo@gmail.com>
2023-09-13 14:48:15 +0200

    Add react-three/drei  KeyboardControls

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/Experience";
+import { KeyboardControls } from "@react-three/drei";
 
 const App = () => {
   return (
     <>
-      <Canvas
-        shadows
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 30,
-          position: [2.5, 4, 6],
-        }}
+      <KeyboardControls
+        map={[
+          { name: "forward", keys: ["ArrowUp", "KeyW"] },
+          { name: "backward", keys: ["ArrowDown", "KeyS"] },
+          { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+          { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+          { name: "jump", keys: ["Space"] },
+          { name: "nextLevel", keys: ["KeyN"] },
+        ]}
       >
-        <Experience />
-      </Canvas>
+        <Canvas
+          shadows
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 30,
+            position: [2.5, 4, 6],
+          }}
+        >
+          <Experience />
+        </Canvas>
+      </KeyboardControls>
     </>
   );
 };
