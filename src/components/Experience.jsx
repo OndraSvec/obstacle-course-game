e89187a489707cec66fa8cf046b4b82f9479ba30
Ondra Svec <ondrasvecveslo@gmail.com>
2023-09-13 14:22:55 +0200

    Add Lights component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,6 +2,7 @@ import { Sky } from "@react-three/drei";
 import { useRef } from "react";
 import Level from "./Level";
 import { Physics } from "@react-three/rapier";
+import Lights from "./Ligths";
 
 const Experience = () => {
   const canJump = useRef(true);
@@ -9,6 +10,7 @@ const Experience = () => {
     <>
       <Sky sunPosition={[0, 3, 2]} />
       <Physics>
+        <Lights />
         <Level canJump={canJump} />
       </Physics>
     </>
---
+++ src/components/Ligths.jsx
@@ -0,0 +1,32 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const Lights = () => {
+  const lightRef = useRef(null);
+
+  useFrame((state) => {
+    lightRef.current.position.z = state.camera.position.z + 1.5 - 4;
+    lightRef.current.target.position.z = state.camera.position.z - 4;
+    lightRef.current.target.updateMatrixWorld();
+  });
+  return (
+    <>
+      <directionalLight
+        ref={lightRef}
+        castShadow
+        position={[4, 4, 1]}
+        intensity={1.5}
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={10}
+        shadow-camera-right={10}
+        shadow-camera-bottom={-10}
+        shadow-camera-left={-10}
+      />
+      <ambientLight intensity={0.5} />
+    </>
+  );
+};
+
+export default Lights;
