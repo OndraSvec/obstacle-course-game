abeff94fb17d617653053a068406e48890a2b0b9
Ondra Svec <ondrasvecveslo@gmail.com>
2023-09-13 14:21:12 +0200

    Add starting block component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,16 @@
 import { Sky } from "@react-three/drei";
 import { useRef } from "react";
 import Level from "./Level";
+import { Physics } from "@react-three/rapier";
 
 const Experience = () => {
   const canJump = useRef(true);
   return (
     <>
       <Sky sunPosition={[0, 3, 2]} />
-      <Level canJump={canJump} />
+      <Physics>
+        <Level canJump={canJump} />
+      </Physics>
     </>
   );
 };
--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,5 +1,35 @@
+import { RigidBody } from "@react-three/rapier";
+import * as THREE from "three";
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const floorMaterial = new THREE.MeshStandardMaterial({ color: "#134e4a" });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "black" });
+
+const BlockStart = ({ position = [0, 0, 0], canJump }) => (
+  <group position={position}>
+    <RigidBody
+      type="fixed"
+      onCollisionEnter={() => {
+        canJump.current = true;
+      }}
+    >
+      <mesh
+        geometry={boxGeometry}
+        material={obstacleMaterial}
+        scale={[4, 0.2, 4]}
+        position-y={-0.1}
+        receiveShadow
+      />
+    </RigidBody>
+  </group>
+);
+
 const Level = () => {
-  return <></>;
+  return (
+    <>
+      <BlockStart />
+    </>
+  );
 };
 
 export default Level;
