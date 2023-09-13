8e5fd941304a2d975ef2add4d0bd6f9032e990ea
Ondra Svec <ondrasvecveslo@gmail.com>
2023-09-13 14:43:51 +0200

    Use gameStore data in the Level component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,15 +3,19 @@ import { useRef } from "react";
 import Level from "./Level";
 import { Physics } from "@react-three/rapier";
 import Lights from "./Ligths";
+import useGameStore from "../useGameStore";
 
 const Experience = () => {
+  const blocksCount = useGameStore((state) => state.blocksCount);
+  const blocksSeed = useGameStore((state) => state.blocksSeed);
+
   const canJump = useRef(true);
   return (
     <>
       <Sky sunPosition={[0, 3, 2]} />
       <Physics debug>
         <Lights />
-        <Level canJump={canJump} />
+        <Level count={blocksCount} seed={blocksSeed} canJump={canJump} />
       </Physics>
     </>
   );
