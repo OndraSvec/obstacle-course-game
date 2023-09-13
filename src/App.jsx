c06ed8d474de273180c3bbd83144f71c28598d09
Ondra Svec <ondrasvecveslo@gmail.com>
2023-09-13 14:14:29 +0200

    Add react-three-fiber Canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Canvas
+        shadows
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 30,
+          position: [2.5, 4, 6],
+        }}
+      >
+        <Experience />
+      </Canvas>
+    </>
+  );
 };
 
 export default App;
